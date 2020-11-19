2aff4ef58124b3a7b69ccbcceb6ffc5e3335554f
coolabhays <abhaysp9955@gmail.com>
2020-11-19 18:15:04 +0530

    handle meta command in seperate function

--- repl4/src/main.c
+++ repl4/src/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "../inc/spdbutil.h"
 
+/**
+  * @brief: handle the execution of meta commands(queries starting with '.'(dot))
+  * @param: created input buffer
+  * @return: status as 0 or 1, whether entered query is meta command or not
+  */
+int handle_meta_commands(InputBuffer* input_buffer);
+
 int main(int argc, char **argv) {
 	/* create new input buffer */
 	InputBuffer* input_buffer = new_input_buffer();
@@ -10,20 +17,8 @@ int main(int argc, char **argv) {
 		print_prompt();  // prompt: spdb >
 		read_input(input_buffer);  // take input with InputBuffer
 
-		if (input_buffer->buffer[0] == '.') {
-			/* Non-SQL statements like ".exit" are called meta command, so
-			 * handling them with seperate function
-
-			 * Using enum result codes wherever possible cause exceptions
-			 are bad(and C doesn't support them)
-			 */
-			switch (do_meta_command(input_buffer)) {
-				case (META_COMMAND_SUCCESS):
-					continue;
-				case (META_COMMAND_UNRECOGNIZED):
-					printf("Unrecognized command '%s'\n", input_buffer->buffer);
-					continue;
-			}
+		if (handle_meta_commands(input_buffer)) {
+			continue;
 		}
 
 		Statement statement;
@@ -49,6 +44,28 @@ int main(int argc, char **argv) {
 			case (EXECUTE_TABLE_FULL):
 				printf("Error: Table full.\n");
 				break;
+		}
+	}
+	return 0;
+}
+
+/**
+  * @brief: handle the execution of meta commands(queries starting with '.'(dot))
+  */
+int handle_meta_commands(InputBuffer* input_buffer) {
+	/* Non-SQL statements like ".exit" are called meta command, so
+	 * handling them with seperate function
+
+	 * Using enum result codes wherever possible cause exceptions
+	 are bad(and C doesn't support them)
+	 */
+	if (input_buffer->buffer[0] == '.') {
+		switch(do_meta_command(input_buffer)) {
+			case (META_COMMAND_SUCCESS):
+				return 1;
+			case (META_COMMAND_UNRECOGNIZED):
+				printf("Unrecognized command '%s'\n", input_buffer->buffer);
+				return 1;
 		}
 	}
 	return 0;
